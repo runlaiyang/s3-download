526dd22ee5fd8bb8f47b247b993b0b00b835f622
Shivendra Panicker <shivendranitb@gmail.com>
2023-06-14 15:17:40 -0700

    Update server.py

--- server.py
+++ server.py
@@ -32,4 +32,4 @@ while(time.time() - loop_start < 90):
 
 if __name__ == '__main__':
     port = int(os.environ.get("FLASK_RUN_PORT", 8000))
-    application.run(host='0.0.0.0', debug=False, port=port)
+    application.run(debug=False, port=port)
