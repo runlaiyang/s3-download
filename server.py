f485ff7fe4412d1433782016053f1aaaec85a228
Shivendra Panicker <shivendranitb@gmail.com>
2023-06-14 23:31:30 -0700

    Update server.py

--- server.py
+++ server.py
@@ -35,4 +35,4 @@ application = Flask(__name__)
 
 if __name__ == '__main__':
     port = int(os.environ.get("FLASK_RUN_PORT", 8000))
-    application.run(debug=False, port=port)
+    application.run(host='0.0.0.0', debug=False, port=port)
